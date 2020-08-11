8f493f7e94e35aea1e311eaa2f9629b4413d03d4
Christo <christo@cryptodrunks.net>
2020-08-11 10:37:21 +0200

    small style edits

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -22,54 +22,48 @@ const Card = ({
     <div className="">
       <div className="card flex flex-column">
         <div className="pa1 ba br3 ">
-          <div className="flex items-center justify-between color-main mr1 ml1 pr2 pl2">
-
-
-          </div>
-
           <div className="cf ph2-ns">
             <div className="fl w-100 w-70-ns pa2">
+              <div className="flex items-center">
+                <img alt={`${title} logo.`} src={logo} />
 
-            <div className="flex items-center">
-              <img alt={`${title} logo.`} src={logo} />
-
-              <h4 className="header4">
-                <Link className="nowrap ma2 hover-bg-transparent" to="/">
-                  {title}
-                </Link>
-              </h4>
-              {isBeta && (
-                <div className="ttu bg-solitude pv1 ph2 br2">Beta</div>
-              )}
-            </div>
-
-
+                <h4 className="header4">
+                  <Link className="nowrap ma2 hover-bg-transparent" to="/">
+                    {title}
+                  </Link>
+                </h4>
+                {isBeta && (
+                  <div className="ttu bg-solitude pv1 ph2 br2">Beta</div>
+                )}
+              </div>
             </div>
             <div className="fl w-100 w-30-ns pa2">
-                <div className="flex items-center justify-between mr1 ml1 pr2 pl2 fr">
+              <div className="flex items-center justify-between mr1 ml1 pr2 pl2 fr">
                 <Link
                   className="btn btn-large btn-bittersweet ml3 mt4"
                   to={installLink}
                 >
                   Install
                 </Link>
-                </div>
+              </div>
             </div>
           </div>
 
-
           <div className="cf ph2-ns">
             <div className="fl w-100 w-70-ns pa2">
-                <div className="flex items-center justify-between color-main mr1 ml1 pr2 pl2">
-                  {children}
-                </div>
+              <div className="flex items-center justify-between color-main mr1 ml1 pr2 pl2">
+                {children}
+              </div>
             </div>
             <div className="fl w-100 w-30-ns pa2">
-                <div className="flex items-center justify-between mr1 ml1 pr2 pl2 fr">
-                  <Link className="btn btn-large btn-more ml3 mt4" to={readmoreLink}>
-                    Read More
-                  </Link>
-                </div>
+              <div className="flex items-center justify-between mr1 ml1 pr2 pl2 fr">
+                <Link
+                  className="btn btn-large btn-more ml3 mt4"
+                  to={readmoreLink}
+                >
+                  Read More
+                </Link>
+              </div>
             </div>
           </div>
         </div>
